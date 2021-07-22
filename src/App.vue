07168bcdd8b379c4c878cac3c4055aa6991fba22
Home Andrey <Andrey.Kholopov.Work@gmail.com>
2021-07-22 07:20:36 +0300

    add button component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
+  <div class="test__wrapper">
+    <test-button textButton='Добавить' />
   </div>
 </template>
 
 <script>
+import Button from './components/Button.vue'
+
 export default {
-  name: 'test_roistat'
+  name: 'test-roistat',
+
+  components: {
+    'test-button': Button
+  }
 }
 </script>
 
 <style>
+.test__wrapper {
+  width: 100%;
+  margin: 25px;
+}
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="button">{{ textButton }}</button>
+</template>
+
+<script>
+export default {
+  name: 'button',
+
+  props: {
+    textButton: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button {
+  background: #ccc;
+  border: 1px solid black;
+  border-radius: 15px;
+  padding: 8px 15px;
+}
+</style>
